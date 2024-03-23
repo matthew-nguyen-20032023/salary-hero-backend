6616cb832515392be967be901e89c3f0c7af330e
dylan123 <123>
2024-03-23 16:34:36 +0700

    test: unit test for new logic init salary job

--- test/mock/mock-company-info-entity.ts
+++ test/mock/mock-company-info-entity.ts
@@ -1,11 +1,15 @@
 import { CompanyInfoEntity } from "src/models/entities/company_info.entity";
-import { generateRandomString } from "src/shares/common";
+import { generateRandomEmail, generateRandomString } from "src/shares/common";
 
-export function mockRandomCompanyInfo(partnerEmail: string): CompanyInfoEntity {
+export function mockRandomCompanyInfo(
+  partnerEmail: string,
+  timezone: number
+): CompanyInfoEntity {
   const newCompanyInfo = new CompanyInfoEntity();
   newCompanyInfo.company_name = generateRandomString();
   newCompanyInfo.company_description = generateRandomString();
-  newCompanyInfo.user_email = generateRandomString();
-  newCompanyInfo.user_email = partnerEmail;
+  newCompanyInfo.timezone = timezone;
+  if (partnerEmail) newCompanyInfo.user_email = partnerEmail;
+  else newCompanyInfo.user_email = generateRandomEmail();
   return newCompanyInfo;
 }
